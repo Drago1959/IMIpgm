cdfb0a64a0bddb71f73b33436607f500d4efc725
Drago1959 <118459346+Drago1959@users.noreply.github.com>
2022-12-05 13:48:48 +0200

    Add files via upload

---
+++ Day1/hw1t2.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int	main () {
+	
+int	w, h;
+	
+	printf("\nPlease, enter with and hight of rectangle:\n");
+	scanf("%d %d", &w, &h);
+	printf("\nThe area of rectangle is: %d\n", w*h );
+	return 0;	
+}
